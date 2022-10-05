2a226664ddd0a834045aa281b80326ab899245e5
Rudra <rudra.behera50@gmail.com>
2022-10-05 12:58:21 +0530

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import "./Header.css";
+import { useState } from "react";
+
+const Header = () => {
+  /*=============== Toggle Menu ===============*/
+  const [Toggle, setToggle] = useState(false);
+
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">
+          Rudra
+        </a>
+
+        <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="#home" className="nav__link active-link">
+                <i className="uil uil-estate nav__icon"></i>Home
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#about" className="nav__link">
+                <i className="uil uil-user nav__icon"></i>About
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#skills" className="nav__link">
+                <i className="uil uil-file-alt nav__icon"></i>Skills
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#skills" className="nav__link">
+                <i className="uil uil-github nav__icon"></i>Projects
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#skills" className="nav__link">
+                <i class="uil uil-docker nav__icon"></i>Blogs
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#skills" className="nav__link">
+                <i className="uil uil-message nav__icon"></i>Contact
+              </a>
+            </li>
+          </ul>
+
+          <i
+            class="uil uil-times nav__close"
+            onClick={() => {
+              setToggle(!Toggle);
+            }}
+          ></i>
+        </div>
+        <div
+          className="nav__toggle"
+          onClick={() => {
+            setToggle(!Toggle);
+          }}
+        >
+          <i class="uil uil-apps"></i>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Home/Social.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/rudra_ooo7/"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="uil uil-instagram"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/rudrabehera/"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="uil uil-linkedin-alt"></i>
+      </a>
+      <a
+        href="https://github.com/Rudra-cs"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
